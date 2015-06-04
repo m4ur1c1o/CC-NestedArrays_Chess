8306e4cb6cd01d55699ae7cd7d0998ed9be18da6
codeamac02 <codeamac02@gmail.com>
2015-06-04 10:58:10 -0500

    Método que crea tablero

--- NestedArray.rb
+++ NestedArray.rb
@@ -1,3 +1,4 @@
+# TABLERO DE AJEDREZ HECHO A MANO
 tablero = [["TN","CN","AN","RN","RN","AN","CN","TN"],
 		   ["PN","PN","PN","PN","PN","PN","PN","PN"],
 		   ["  ","  ","  ","  ","  ","  ","  ","  "],
@@ -9,17 +10,32 @@ tablero = [["TN","CN","AN","RN","RN","AN","CN","TN"],
 		  ]
 
 
-# puts "-------------------------------------------------------------------------"
-# puts "|" + tablero[0][0] + "|" + tablero[0][1] + "|" + tablero[0][0] + "|" + tablero[0][1] + "|" + tablero[0][0] + "|" + tablero[0][1] + "|" + tablero[0][0] + "|" + tablero[0][1] + "|"
+# # puts "-------------------------------------------------------------------------"
+# # puts "|" + tablero[0][0] + "|" + tablero[0][1] + "|" + tablero[0][0] + "|" + tablero[0][1] + "|" + tablero[0][0] + "|" + tablero[0][1] + "|" + tablero[0][0] + "|" + tablero[0][1] + "|"
 
-tablero.each do |x|
-	# print "| #{x} |\n "
-	# puts "|"
-	puts "\n-----------------------------------------------"
-	x.each do |y|
-		# puts y
-		print " #{y} | "
-	end
+# tablero.each do |x|
+# 	puts "\n-----------------------------------------------"
+# 	print "|"
+# 	x.each do |y|
+# 		# puts y
+# 		print " #{y} | "
+# 	end
 	
+# end
+# puts "\n-----------------------------------------------"
+
+# METODO QUE RECIBE UN ARRAY Y REGRESA UN TABLERO DE AJEDREZ
+def crea_tablero(array)
+	array.each do |x|
+		puts "\n-----------------------------------------"
+		print "| "
+		x.each do |y|
+			# puts y
+
+			print "#{y} | "
+		end
+	end
+	puts "\n-----------------------------------------"
 end
-puts "\n-----------------------------------------------"
+
+crea_tablero(tablero)
